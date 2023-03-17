c8b6b3a8faabe73bceed3e3e8a8b5cb5af2167df
Qrroll <dainsureivu@gmail.com>
2023-03-17 18:10:52 +0200

    change/Store

--- src/aso_lab2/Store.java
+++ src/aso_lab2/Store.java
@@ -1,6 +1,6 @@
 package aso_lab2;
 
-public class Store {
+public class Store { //by Prisacari
     
     private static final int capacity = 0; // указываете в соответствии с вариантом
     private int quantity;
@@ -10,11 +10,16 @@ public class Store {
     }
     
     public synchronized int get() {
-        return 0; // тип данных возвращаемого значения можно изменить
+        if( quantity - 1 < 0 ) {
+            quantity-=1;
+        }
+        return quantity; // type int
     }
     
     public synchronized void put() {
-        
+        if( quantity + 1 > capacity ) {
+            quantity+=1;
+        } else System.out.println("Store is overflow");
     }
     
 }
