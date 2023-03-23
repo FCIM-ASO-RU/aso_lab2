d0faaac903d797525e46913e93d887aec10e7db4
whereareyoo <ramin-gashimov@mail.ru>
2023-03-23 22:03:18 +0200

    -Producer class implementation by Ramin Gashimov

--- src/aso_lab2/Producer.java
+++ src/aso_lab2/Producer.java
@@ -1,18 +1,31 @@
 package aso_lab2;
+import java.util.Random;
 
 public class Producer extends Thread {
-    /* Эту хрень делает Рамин */
-
     private final Store store;
-    
-    public Producer(final Store store,final String name) {
-        this.store = store;
-        setName(name);
+    private final int number;
+    private final String[] vawel;
+    private final int size;
+
+    public Producer(final Store c, final int number, final int size) {
+        store = c;
+        this.number = number;
+        this.size = size;
+        vawel = new String[]{"Lipsi HA", "Za деньги да", "KAK MOMMY", "POPSTAR", "MONEYKEN LOVE"};
     }
-    
-    @Override
+
     public void run() {
-        throw new UnsupportedOperationException();
+        final Random random = new Random();
+        for (int i =0; i < size ; i++){
+            final int arrayElement = random.nextInt(vawel.length);
+            store.put(vawel[arrayElement]);
+            System.out.println("Producer #" + number + " put: " + vawel[arrayElement]);
+            try {
+                sleep((int) (Math.random() * 100));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
-    
 }
