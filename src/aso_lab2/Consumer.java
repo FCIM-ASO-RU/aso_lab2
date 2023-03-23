8d031a7a6eef5b13b489315cf67050febb814571
Mihail2504 <miha-25.01@mail.ru>
2023-03-23 22:56:23 +0200

    Initial commit

--- src/aso_lab2/Consumer.java
+++ src/aso_lab2/Consumer.java
@@ -1,17 +1,40 @@
 package aso_lab2;
 
+import static aso_lab2.Store.quantity;
+
 public class Consumer extends Thread {
     
     private final Store store;
+    private static int cons = 0;
     
-    public Consumer(Store _store, String name) {
+    public Consumer(Store _store, String _name) {
         store = _store;
-        setName(name);
+        setName(_name);
     }
     
     @Override
     public void run() {
-        
+        // Так как было сгенерировано 47 объектов то и потреблено столько же
+        while(cons<=quantity){
+
+            try {
+                //ожидание пока другой поток не выполнит нужную работу
+                Thread.sleep(150);
+            } catch (InterruptedException ex) {
+                System.out.println(ex);
+            }
+
+            //Код выполняемый в synchronized выполняется одним объектом и этот объект держит его ключ, пока объект не отдаст ключ никто не может выполнять этот же код
+            synchronized (this){
+                store.get(getName()); //Потребитель потребляет два объекта
+                store.get(getName());
+                cons+=2;
+                System.out.println("Consumers got " + cons);
+            }
+
+        }
+        System.out.println('\n'+"-->" + getName() + "  stopped");
     }
-    
+
 }
+
--- src/aso_lab2/Main.java
+++ src/aso_lab2/Main.java
@@ -3,9 +3,26 @@ package aso_lab2;
 public class Main {
 
     public static void main(String[] args) {
+        //Создаем объект Магазина
         Store store = new Store();
-        
-        
+        //По заданию два производителя и их наименования
+        Producer producer1 = new Producer(store, "Producer-1");
+        Producer producer2 = new Producer(store, "Producer-2");
+
+        //По заданию 4 потребителя
+        Consumer consumer1 = new Consumer(store, "Consumer-1");
+        Consumer consumer2 = new Consumer(store, "Consumer-2");
+        Consumer consumer3 = new Consumer(store, "Consumer-3");
+        Consumer consumer4 = new Consumer(store, "Consumer-4");
+
+        //Thread class имеет метод start, который запускает в отдельном потоке код написанный в методе run
+        producer1.start();
+        producer2.start();
+
+        consumer1.start();
+        consumer2.start();
+        consumer3.start();
+        consumer4.start();
     }
     
 }
