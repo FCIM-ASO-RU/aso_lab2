c36ff29df2c4b6c3ada40e0d30e5c2beb7359550
Qrroll <dainsureivu@gmail.com>
2023-03-24 15:50:58 +0200

    change/Store

--- src/aso_lab2/Store.java
+++ src/aso_lab2/Store.java
@@ -1,25 +1,46 @@
 package aso_lab2;
 
+class struct{
+    public boolean error;
+    public String message;
+    public struct(){
+        error = false;
+        message = "";
+    }
+}
 public class Store { //by Prisacari
     
-    private static final int capacity = 0; // указываете в соответствии с вариантом
-    private int quantity;
+    private static final int capacity = 5; // обьем склада
+    private int quantity; // склад
     
     public Store() {
         quantity = 0;
     }
     
-    public synchronized int get() {
-        if( quantity - 1 < 0 ) {
+    public synchronized struct get() {
+        struct get_out = new struct();
+        if( quantity - 1 >= 0 ) {
             quantity-=1;
+            get_out.message += "Get from Store, ";
+        } else {
+            get_out.error = true;
+            get_out.message += "Store is empty, ";
         }
-        return quantity; // type int
+        get_out.message += "store current: " + Integer.toString(quantity) + ".";
+        return get_out;
     }
     
-    public synchronized void put() {
-        if( quantity + 1 > capacity ) {
+    public synchronized struct put() {
+        struct put_out = new struct();
+        if( quantity + 1 <= capacity ) {
             quantity+=1;
-        } else System.out.println("Store is overflow");
+            put_out.message += "Put to Store, ";
+        } else {
+            put_out.error = true;
+            put_out.message += "Store is overflow, ";
+        }
+        put_out.message += "store current: " + Integer.toString(quantity) + ".";
+        return put_out;
     }
     
 }
