52006dbaede43e50c927f7bafa97b04c7c91497c
Bizgrell <dratutiyawkaf@gmail.com>
2023-03-30 21:27:11 +0300

    consumer

--- src/aso_lab2/Consumer.java
+++ src/aso_lab2/Consumer.java
@@ -1,35 +1,46 @@
 package aso_lab2;
 
 public class Consumer extends Thread {
-    
+
     private final Store store;
     private int performance;
+    private int consumedByMe;
     public Consumer(Store _store, String name) {
         store = _store;
         setName(name);
         performance = 1;
+        consumedByMe = 0;
     }
-    public void setPerfomance(int perf){
+    public void setPerformance(int perf){
         performance = perf;
     }
-    
+
     @Override
     public void run() {
-        struct info = new struct();
+        struct info;
         while (true) {
-            info = store.get(performance);
-            if (info.error) {
-                try {
-                    System.out.println(getName() + ": " + info.message);
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+            synchronized (this) {
+                info = store.get(performance);
+                if (!info.error) {
+                    consumedByMe += performance;
                 }
-            } else System.out.println(getName() + ": " + info.message);
-            if ( info.stopSignal ) {
-                break;
+
+                System.out.printf("%s: %-18s | Consumed by me: %2d | Consumed by all: %2d  | Store current: %2d \n", getName() , info.message , consumedByMe, info.fromAll, info.storeCurrent);
+
+                if (info.error) {
+                    try {
+                        Thread.sleep(30);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+                if ( info.stopSignal ) {
+                    System.out.println(getName() + ": thread stop | consumed by me: " + consumedByMe);
+                    break;
+                }
             }
         }
     }
-    
+
 }
